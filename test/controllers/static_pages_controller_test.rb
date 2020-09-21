436e5c64214e7e62afafc24652459cb8ca11339e
Shirley Hastier <shastier@yahoo.com>
2020-09-21 12:46:57 -0500

    Tests ok, added title to static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,10 +4,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Budget Friend App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Budget Friend App"
   end
 end
