c56de7049da87409fccc29727ae52ab95967d9c4
Shirley Hastier <shastier@yahoo.com>
2020-09-23 18:43:37 -0500

    Client: Home page

---
+++ app/javascript/expenses/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+  return (
+    <header className="header headernav">
+        <div className="logo">Budget App</div>
+    </header>    
+  );
+};
+
+export default Header;
