025d5dd61b7cdb3e9ca030d923dae678b1cad61b
Shirley Hastier <shastier@yahoo.com>
2020-09-22 19:25:41 -0500

    Added routes categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  # Categories route: /api/v1/categories
+  namespace :api do
+    namespace :v1 do
+      resources :categories
+    end
+  end
+
+    # Expenses route: /api/v1/expenses
+    namespace :api do
+      namespace :v1 do
+        resources :expenses
+      end
+    end
+
 end
