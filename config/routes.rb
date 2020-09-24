4e9d4fe260b29d8c6353094bc25a84005f1a6fd6
Shirley Hastier <shastier@yahoo.com>
2020-09-24 11:50:47 -0500

    Auth, added routes login / logout / profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "static_pages#home"
 
+  #  map SessionsController actions to /login and /logout
+  post   "/login"       => "sessions#create"
+  delete "/logout"      => "sessions#destroy"
+  get "/profile"        => "users#profile"
+  resources :users
+
   # Categories route: /api/v1/categories
   namespace :api do
     namespace :v1 do
