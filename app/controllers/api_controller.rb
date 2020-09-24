2ab1a7014967bf9d7fe2d1fced042430e8c077a0
Shirley Hastier <shastier@yahoo.com>
2020-09-24 11:47:40 -0500

    Auth, Sessions / Api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,24 @@
 class ApiController < ApplicationController
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+
+    def require_login
+      authenticate_token || render_unauthorized("Access denied")
+    end
+  
+    def current_user      
+       @current_user ||= authenticate_token
+    end
+  
+    protected
+  
+    def render_unauthorized(message)
+      errors = { errors: [ detail: message ] }
+      render json: errors, status: :unauthorized
+    end
+  
+    private
+  
+    def authenticate_token
+      authenticate_with_http_token { | token, options | User.find_by(auth_token: token) }
+    end
 end
