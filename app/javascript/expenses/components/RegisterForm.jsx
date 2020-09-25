261e864416e5d8507b1a70b5637293232532aa02
Shirley Hastier <shastier@yahoo.com>
2020-09-25 14:33:23 -0500

    Registration page

--- app/javascript/expenses/components/RegisterForm.jsx
+++ app/javascript/expenses/components/RegisterForm.jsx
@@ -1,11 +1,76 @@
 import React from 'react';
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const RegisterForm = (props) => {
-  return (
-    <div>
-        <h2>Register page</h2>
-    </div>
-  );
-};
+class RegisterForm extends Component {
+    constructor(){
+        super();
+        this.state = {
+            first_name: '',
+            last_name: '',
+            email: '',
+            username: '',
+            password: '',
+        }
+    }
+
+    handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    render(){
+        return(
+            <div className="loginform">
+                <form className="loginform" onSubmit={(e) => this.props.handleRegisterSubmit(e, this.state)}>
+                    <input 
+                        type="text" 
+                        name="first_name" 
+                        value={this.state.first_name} 
+                        placeholder="First Name" 
+                        onChange={this.handleChange} 
+                    />
+
+                    <input 
+                        type="text" 
+                        name="last_name" 
+                        value={this.state.last_name} 
+                        placeholder="Last Name" 
+                        onChange={this.handleChange} 
+                    />
+
+                    <input 
+                        type="email" 
+                        name="email" 
+                        value={this.state.email} 
+                        placeholder="email" 
+                        onChange={this.handleChange} 
+                    />
+
+                    <input 
+                        type='text'
+                        name='username'
+                        value={this.state.username}
+                        placeholder='Username'
+                        onChange={this.handleChange} 
+                    />
+
+                    <input 
+                        type='password'
+                        name='password'
+                        value={this.state.password}
+                        placeholder='Password'
+                        onChange={this.handleChange} 
+                    />
+
+                    <input type='submit' value='Sign Up!'/>
+                </form>
+            </div>
+        )
+    }
+}
 
 export default RegisterForm;
