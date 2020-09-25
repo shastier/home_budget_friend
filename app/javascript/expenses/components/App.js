c302073df6f01bd8d4c2dec19175d1686ca7f7ec
Shirley Hastier <shastier@yahoo.com>
2020-09-25 13:23:31 -0500

    F

---
+++ app/javascript/expenses/components/App.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+import Auth from '../Auth';
+
+import Header from './Header';
+import Home from './Home';
+
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
+import Dashboard from './Dashboard';
+
+import AllExpenses from './AllExpenses';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      auth: Auth.isUserAuthenticated(),
+      userData: null,
+      expenses: null,      
+    }
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  componentDidMount() {    
+    fetch('/profile', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: `${Auth.getToken()}`,
+      }
+    }).then(res => res.json())
+    .then(res => {
+      this.setState({
+        userData: res.user,
+        userDataLoaded: true,
+        userPostExpenses: res.post_expenses,
+        userPostExpensesLoaded: true,
+      })
+    })
+  }
+
+  handleLoginSubmit(e) {
+    e.preventDefault();
+    fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: this.state.loginUserName,
+        password: this.state.loginPassword,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(res => res.json())
+    .then(res => {
+      console.log(res);
+      if (res.token) {
+        Auth.authenticateToken(res.token);
+        this.setState({
+          auth: Auth.isUserAuthenticated(),
+          loginUserName: '',
+          loginUserPassword: '',
+        })
+      }
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  handleRegisterSubmit(e) {
+    e.preventDefault();
+    fetch('/users', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: {
+          username: this.state.registerUserName,
+          password: this.state.registerPassword,
+          email: this.state.registerEmail,
+          name: this.state.registerName,
+        }
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(res => res.json())
+    .then(res => {
+      if (res.token) {
+        Auth.authenticateToken(res.token);
+        this.setState({
+          auth: Auth.isUserAuthenticated(),
+        })
+      }
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  logout() {
+    fetch('/logout', {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token ${Auth.getToken()}`,
+        token: Auth.getToken(),
+      }
+    }).then(res => {
+      Auth.deauthenticateUser();
+      this.setState({
+        auth: Auth.isUserAuthenticated(),
+        loginUserName: '',
+        loginUserPassword: '',
+      })
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+            <Header /> 
+            <div className="container">
+                <Route exact path='/' 
+                    render={() => ( <Home /> )} />    
+            </div>
+        </div>        
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ app/javascript/expenses/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <div className="home">
+        <h1>Home Budget Friend</h1>
+    </div>
+  );
+};
+
+export default Home;
