7a2bdc9efae49e292d4ba703d1f996c5b1d1c205
Shirley Hastier <shastier@yahoo.com>
2020-09-28 08:06:09 -0500

    Styling, push to heroku

--- app/javascript/expenses/components/LoginForm.jsx
+++ app/javascript/expenses/components/LoginForm.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './App.css'
 
+import './App.css'
 
 class LoginForm extends Component {
     constructor(){
