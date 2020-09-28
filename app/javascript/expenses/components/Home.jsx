0f1078dec7e901bd8ed862d24515a016a4ed61d1
Shirley Hastier <shastier@yahoo.com>
2020-09-28 11:25:28 -0500

    About page ok

--- app/javascript/expenses/components/Home.jsx
+++ app/javascript/expenses/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = (props) => {
   return (
     <div className="home">
         <h1>Welcome to "Home Budget Friend App!"</h1>
-        <h2>Please, check the "About" page to see and step by step guide of how to use it.</h2>
+        <h2>Please, check the "About" page to see a step by step guide of how to use the app.</h2>
     </div>
   );
 };
